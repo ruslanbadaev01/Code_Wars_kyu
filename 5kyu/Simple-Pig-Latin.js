1b4b8e4bac4cc6e8ebe340391e51e8390bff97cf
L0c_d0G <94112336+ruslanbadaev01@users.noreply.github.com>
2023-01-30 16:01:29 +0300

    5kyu

---
+++ 5kyu/Simple-Pig-Latin.js
@@ -0,0 +1,13 @@
+function pigIt(str) {
+  const splitWord = str.split(" ");
+  const map1 = splitWord.map((i) => i.split(""));
+  for (i = 0; i < map1.length; i++) {
+    let firstLet = map1[i].shift();
+    map1[i].push(firstLet + "ay");
+  }
+
+  const map2 = map1.map((i) => i.join(""));
+
+  const result = map2.join(" ");
+  return result;
+}
